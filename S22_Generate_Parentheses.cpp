4b7e606f216087e886faa4677ebec588c56bd48e
spiderbait <dontwakeupbabys@hotmail.com>
2020-04-21 22:09:08 +0800

    finished s22

--- S22_Generate_Parentheses.cpp
+++ S22_Generate_Parentheses.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stack>
 
 using namespace std;
 
@@ -8,21 +9,37 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> result;
-        char leftParenthes = '(';
-        char rightParenthes = ')';
-        int leftCount = n;
-        int rightCount = n;
- 
-        cout << leftParenthes + rightParenthes << endl;
-
+        generate(n, n, "", result);
         return result;
+    }
+
+    void generate(int left, int right, string s, vector<string> &result) {
+        // cout << "left: " << left << ", right: " << right << endl;
+        if(left == 0 && right == 0) {
+            result.push_back(s);
+            return;
+        }
+        if(left > 0) {
+            generate(left - 1, right, s + "(", result);
+        }
+        if(left < right) {
+            generate(left, right - 1, s + ")", result);
+        }
+    }
+
+    void print(vector<string> result) {
+        for (int i = 0; i < result.size(); i++)
+        {
+            cout << result.at(i) << endl;
+        }
+        
     }
 };
 
 int main() {
 
     Solution s;
-    s.generateParenthesis(3);
+    s.print(s.generateParenthesis(4));
 
     return 0;
 }
