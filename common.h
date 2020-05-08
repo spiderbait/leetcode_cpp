04a32bc139ada6cc98ccae521e6739b0b37b7660
spiderbait <dontwakeupbabys@icloud.com>
2020-05-08 23:42:55 +0800

    add header file

---
+++ common.h
@@ -0,0 +1,12 @@
+#include "ListNode.h"
+
+using namespace std;
+
+template<class T>
+int getArraySize(T &array) {
+    return sizeof(array)/sizeof(array[0]);
+}
+
+ListNode* arrayToLinkList(int n[]) {
+    
+}
