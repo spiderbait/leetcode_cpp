5b9d56f9e6e8e6250b068156756ae384cd910312
spiderbait <dontwakeupbabys@hotmail.com>
2020-05-08 17:32:03 +0800

    add s 6

--- S6_ZigZag_Conversion.cpp
+++ S6_ZigZag_Conversion.cpp
@@ -5,6 +5,17 @@ using namespace std;
 class Solution {
 public:
     string convert(string s, int numRows) {
+        int n = numRows;
+        for (int i = 0; i < s.length(); i++) {
+            
+        }
         
     }
 };
+
+int main() {
+    string s = "PAYPALISHIRING";
+    int num = 3;
+
+
+}
