a8a5d0d23f123efddb8b71d8cd554b298fd4266a
spiderbait <dontwakeupbabys@icloud.com>
2020-05-14 22:27:07 +0800

    accepted

--- S1323_Maximum_69_Number.cpp
+++ S1323_Maximum_69_Number.cpp
@@ -15,15 +15,25 @@ public:
             n /= 10;
         }
 
-        return (int)pow(10, int(log10(max - num)))*3 + num;
+        int m = max - num;
+        int bit_m = 0;
+        while(m) {
+            bit_m ++;
+            m /= 10;
+        }
+        return (int)pow(10, bit_m-1)*3 + num;
+        // return (int)pow(10, int(log10(max - num)))*3 + num;
     }
 };
 
+
+
 int main() {
     Solution s;
-    cout << s.maximum69Number(9999) << endl;;
+    cout << s.maximum69Number(9) << endl;;
 }
 
+// TODO: if return in log form, then throw runtime error like below:
 // Runtime Error Message:
 // Line 14: Char 33: runtime error: -inf is outside the range of representable values of type 'int' (solution.cpp)
 // Last executed input:
